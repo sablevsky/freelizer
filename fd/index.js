f0df48eaa0943af53f5368bd662ff1f215fb8dbd
sablevsky <vladislav1145@gmail.com>
2020-12-07 22:32:38 +0300

    feat: created lib interaction interface

--- fd/index.js
+++ fd/index.js
@@ -4,46 +4,42 @@ import { USER_MEDIA_CONSTRAINTS, FFT_SIZE } from './constants.js'
 import { autoCorrelate } from './algorithm.js'
 import { getNoteDataFromFrequency } from './helpers.js'
 
-const init = ({ audioContext, analyser, rafID, callback }) => {
-  return new Promise((resolve) => {
-    navigator.mediaDevices
-      .getUserMedia(USER_MEDIA_CONSTRAINTS)
-      .then((stream) => {
-        audioContext = new AudioContext()
-        analyser = audioContext.createAnalyser()
-        analyser.fftSize = FFT_SIZE
-        audioContext.createMediaStreamSource(stream).connect(analyser)
-        resolve({ audioContext, analyser, rafID, callback })
-      })
-      .catch((error) => console.error({ error }))
-  })
-}
-
-const update = ({ audioContext, analyser, rafID, callback }) => {
-  const buffer = new Float32Array(FFT_SIZE)
-  analyser.getFloatTimeDomainData(buffer)
-  const frequency = autoCorrelate(buffer, audioContext.sampleRate)
-  frequency && callback(getNoteDataFromFrequency(frequency))
-  rafID.value = requestAnimationFrame(
-    update.bind(null, { audioContext, analyser, rafID, callback })
-  )
-}
-
-const frequencyDetector = (callback) => {
-  const rafID = {}
+export const frequencyDetector = async () => {
+  let rafID
   let audioContext
   let analyser
+  let callbacks = []
 
-  return [
-    () => init({ audioContext, analyser, rafID, callback }),
-    update,
-    () => window.cancelAnimationFrame(rafID.value),
-  ]
+  const init = async () => {
+    try {
+      await navigator.mediaDevices
+        .getUserMedia(USER_MEDIA_CONSTRAINTS)
+        .then((stream) => {
+          audioContext = new AudioContext()
+          analyser = audioContext.createAnalyser()
+          analyser.fftSize = FFT_SIZE
+          audioContext.createMediaStreamSource(stream).connect(analyser)
+        })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const update = () => {
+    const buffer = new Float32Array(FFT_SIZE)
+    analyser.getFloatTimeDomainData(buffer)
+    const frequency = autoCorrelate(buffer, audioContext.sampleRate)
+    frequency &&
+      callbacks.forEach((fn) => fn(getNoteDataFromFrequency(frequency)))
+    rafID = requestAnimationFrame(update)
+  }
+
+  await init()
+
+  return {
+    start: () => update(),
+    stop: () => cancelAnimationFrame(rafID),
+    subscribe: (fn) => (callbacks = [...callbacks, fn]),
+    unsubscribe: (fn) => (callbacks = callbacks.filter((el) => el !== fn)),
+  }
 }
-
-;(async function () {
-  const [init, start, stop] = frequencyDetector(console.log)
-  const res = await init()
-  document.querySelector('#start').addEventListener('click', () => start(res))
-  document.querySelector('#stop').addEventListener('click', stop)
-})()
